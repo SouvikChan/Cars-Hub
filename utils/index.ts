72cb03fd923520ab454e0018f718ea80322ff2ad
Souvik Chandra <souvikchandra9454@gmail.com>
2023-11-28 23:48:04 +0530

    error

--- utils/index.ts
+++ utils/index.ts
@@ -5,6 +5,7 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const mileageFactor = 0.1; // Additional rate per mile driven
   const ageFactor = 0.05; // Additional rate per year of vehicle age
 
+  
   // Calculate additional rate based on mileage and age
   const mileageRate = city_mpg * mileageFactor;
   const ageRate = (new Date().getFullYear() - year) * ageFactor;
